4dd0d88500ade59f88272c854aef2edd7a4ebe53
bceaton <brett.eaton@ubc.ca>
2019-01-22 14:59:11 -0800

    Updates to the bicalc package have been made to follow the approach recommended by Huber

--- R/bicalc_functions.r
+++ R/bicalc_functions.r
@@ -1,54 +1,3 @@
-#' Calculate the percentiles and confidence intervals
-#'
-#' \code{QuantCI} is a function that uses cumulative frequency distribution
-#' data for Wolman or grid-by-number samples of bed surface texture to
-#' estimate the value of user-specified percentiles. The function also uses
-#' binomial theory to estimate the confidence intervals corresponding to a
-#' user-specified confidence level, based on the number of grain size
-#' measurements that were used to construct the cumulative frequency
-#' distribution.
-#'
-#' The function returns a data frame listing the estimate of each quantile,
-#' the upper limit, and the lower limit. This method is applicable to all
-#' quantiles between the 5th and 95th percentile of the distirbution, but
-#' not beyond that.
-#'
-#' @param cfd data frame providing a list grain sizes in the first variable
-#' and the corresponding cumulative proportion finer in the second. The
-#' grain sizes should be recorded in mm, and the proportion finer in [0,1].
-#' @param n total number of observations upon which the cumulative
-#' frequency distribution in \code{cfd} is based
-#' @param probs numeric vector of probabilities with values in [0,1].
-#' @param Np if the cumulative frequency data was a sub-sample from a finite
-#' population of size Np, then the confidence range will be smaller and a
-#' finite population correction is applied.
-#' @param alpha  the desired confidence level for which to calculate a
-#' confidence interval in [0,1].
-#' @export
-QuantCI = function(cfd, n, probs = seq(0.05,0.95,0.01), Np = NA, alpha = 0.05){
-  tcrit = qt(1 - alpha/2, n)  #calculate t statistic
-  #  calculate binomial standard deviation
-  if (is.na(Np)){
-    sigma = sqrt(n*probs*(1-probs))
-  }else if(!is.na(Np)){
-    fpc = sqrt ((Np - n) / (Np - 1))
-    sigma = sqrt(n*probs*(1-probs))*fpc
-  }
-  # estimate percentiles
-  phi = log2(cfd[[1]])
-  X = cfd[[2]]
-  estimate = 2^approx(x = X, y = phi, xout = probs, rule = 2)[[2]]
-  p.upper = (n*probs + tcrit*sigma)/n
-  upper = 2^approx(x = X, y = phi, xout = p.upper, rule = 2)[[2]]
-  p.lower = (n*probs - tcrit*sigma)/n
-  lower = 2^approx(x = X, y = phi, xout = p.lower, rule = 2)[[2]]
-
-  results = data.frame(probs, estimate, lower, upper)
-
-  return(results)
-}
-
-
 #' Generate a data frame containing cumulative frequency distribution data
 #'
 #' \code{MakeCFD} is a function that creates a data frame containing the
@@ -81,12 +30,12 @@ MakeCFD = function(obs, increment = 0.5, count = FALSE, plot = FALSE){
                  plot = FALSE)
   cfd = data.frame(size)
   cfd$probs = c(0, cumsum(results$counts))/sum(results$counts)
-
+  
   if(count == TRUE){
     cfd$counts = c(0,results$counts)
     cfd = (cfd[which(!cfd$counts == 0),])
   }
-
+  
   if(plot == TRUE){
     plot(cfd[[1]], cfd[[2]],
          type = "b",
@@ -95,9 +44,134 @@ MakeCFD = function(obs, increment = 0.5, count = FALSE, plot = FALSE){
          xlab = "grain size (mm)",
          ylab = "cum. prop. finer")
   }
-
+  
   return(cfd)
 }
+
+#' Calculate the confidence interval for a quantile 'p' 
+#' 
+#' \code{QuantBD} uses the  binomial distribution to compute a 
+#' near-symmetric distribution-free confidence interval for a quantile 'p'.
+#' Returns indices for the order statistics, along with coverage probability.
+#' This script was adapted by R.D. Moore from one found at
+#'   @MISC {284970,
+#'     TITLE = {How to obtain a confidence interval for a percentile?},
+#'    AUTHOR = {whuber (https://stats.stackexchange.com/users/919/whuber)},
+#'     HOWPUBLISHED = {Cross Validated},
+#'     NOTE = {URL:https://stats.stackexchange.com/q/284970 (version: 2017-06-12)},
+#'     EPRINT = {https://stats.stackexchange.com/q/284970},
+#'     URL = {https://stats.stackexchange.com/q/284970}
+#'     
+#'The function returns the indices representing a a confidence interval 
+#'that contains the population quantile of interest, assuming a given
+#'confidence level. The script also returns the probability coverage 
+#'for the confidence interval. Finally, the function returns an approximation
+#'of the confidence interval that has equal areas in the tails of the distribution
+#'
+#'@param n the sample size 
+#'@param p the desired quantile to be estimated from the sample
+#'@param alpha the confidence level (default value is 0.05)
+#'@export
+#'
+
+QuantBD <- function(n, p, alpha = 0.05) {
+  #
+  # Search over a small range of upper and lower order statistics for the 
+  # closest coverage to 1 - alpha (but not less than it, if possible).
+  #   n = sample size
+  #   p = desired quantile
+  # 
+  u <- qbinom(1 - alpha/2, n, p) + (-2:2) + 1
+  l <- qbinom(alpha/2, n, p) + (-2:2)
+  u[u > n] <- Inf
+  l[l < 0] <- -Inf
+  p_c <- outer(l, u, function(a, b) pbinom(b-1, n, p) - pbinom(a-1, n, p))
+  if (max(p_c) < 1 - alpha) {
+    i <- which(p_c == max(p_c)) 
+  } else {
+    i <- which(p_c == min(p_c[p_c >= 1 - alpha]))
+  }
+  i <- i[1]
+  #
+  # return the order statistics and the actual coverage
+  #
+  u <- rep(u, each = 5)[i]
+  l <- rep(l, 5)[i]
+  
+  # now estimate a CI with equal tails
+  # find cumulative probability and set alpha
+  k = 1:n
+  pcum = pbinom(k, n , p) 
+  
+  # use approx function to interpolate the "k" values associated with the limits
+  lu_approx = approx(x = pcum, y = k, xout = c(alpha/2, 1 - alpha/2))$y
+  
+  list(interval = c(l, u), coverage = p_c[i], equaltail = lu_approx)
+}
+
+
+#' Calculate the percentiles and confidence intervals
+#'
+#' \code{WolmanCI} is a function that uses cumulative frequency distribution
+#' data for Wolman or grid-by-number samples of bed surface texture to
+#' estimate the value of user-specified percentiles. The function also uses
+#' the normal approximation to estimate the confidence intervals corresponding 
+#' to a user-specified confidence level, based on the number of grain size
+#' measurements that were used to construct the cumulative frequency
+#' distribution.
+#'
+#' The function returns a data frame listing the estimate of each quantile,
+#' the upper limit, and the lower limit. This method is applicable to all
+#' quantiles between the 5th and 95th percentile of the distirbution, but
+#' not beyond that.
+#'
+#' @param cfd data frame providing a list grain sizes in the first variable
+#' and the corresponding cumulative proportion finer in the second. The
+#' grain sizes should be recorded in mm, and the proportion finer in [0,1].
+#' @param n total number of observations upon which the cumulative
+#' frequency distribution in \code{cfd} is based
+#' @param probs numeric vector of probabilities with values in [0,1].
+#' @param equaltail is a logical variable that determines whether the calculations
+#' of the confidence interval are based on an approximation with equal areas in
+#' each tail (the default), or based on the exact binomial solution with a coverage
+#' of at least 95%
+#' @param Np if the cumulative frequency data was a sub-sample from a finite
+#' population of size Np, then the confidence range will be smaller and a
+#' finite population correction is applied.
+#' @param alpha  the desired confidence level for which to calculate a
+#' confidence interval in [0,1].
+#' @export
+WolmanCI = function(cfd, n,  probs = seq(0.05, 0.95, 0.05), equaltail = T,  Np = NA, alpha = 0.05){
+  # use the binomial approach
+  p.upper = vector(mode="numeric", length = length(probs))
+  p.lower = vector(mode="numeric", length = length(probs))
+  if (equaltail == T){
+    for(i in seq_along(probs)){
+      tmp = QuantBD(n, probs[i], alpha)
+      p.upper[i] = tmp$equaltail[2]/n
+      p.lower[i] = tmp$equaltail[1]/n
+    }
+  }else{
+    for(i in seq_along(probs)){
+      tmp = QuantBD(n, probs[i], alpha)
+      p.upper[i] = tmp$interval[2]/n
+      p.lower[i] = tmp$interval[1]/n
+    }
+  }
+    
+  # estimate percentiles
+  phi = log2(cfd[[1]])
+  X = cfd[[2]]
+  estimate = 2^approx(x = X, y = phi, xout = probs, rule = 2)[[2]]
+  upper = 2^approx(x = X, y = phi, xout = p.upper, rule = 2)[[2]]
+  lower = 2^approx(x = X, y = phi, xout = p.lower, rule = 2)[[2]]
+  results = data.frame(probs, estimate, lower, upper)
+
+  return(results)
+}
+
+
+
 
 #' Generate a polygon representing the confidence bounds for a grain size distribution
 #'
@@ -115,6 +189,10 @@ MakeCFD = function(obs, increment = 0.5, count = FALSE, plot = FALSE){
 #' frequency distribution in \code{cfd} is based
 #' @param probs numeric vector of probabilities defining the polygon vertices,
 #'  with values in [0,1].
+#' @param equaltail is a logical variable that determines whether the calculations
+#' of the confidence interval are based on an approximation with equal areas in
+#' each tail (the default), or based on the exact binomial solution with a coverage
+#' of at least 95%
 #' @param Np if the cumulative frequency data was a sub-sample from a finite
 #' population of size Np, then the confidence range will be smaller and a
 #' finite population correction is applied.
@@ -123,24 +201,48 @@ MakeCFD = function(obs, increment = 0.5, count = FALSE, plot = FALSE){
 #' @param plot optional flag to produce a graph of the resulting grain size
 #' distribution
 #' @export
-PolyCI = function(cfd, n, probs = seq(0.05, 0.95, 0.01), Np= NA, alpha = 0.05, plot = FALSE){
-  tcrit = qt(1 - alpha/2, n)  #calculate t statistic
-  #  calculate binomial standard deviation
-  if (is.na(Np)){
-    sigma = sqrt(n*probs*(1-probs))
-  }else if(!is.na(Np)){
-    fpc = sqrt ((Np - n) / (Np - 1))
-    sigma = sqrt(n*probs*(1-probs))*fpc
+PolyCI = function(cfd, n, probs = seq(0.01, .99, 0.01), equaltail = T, Np= NA, alpha = 0.05, plot = FALSE){
+  # tcrit = qt(1 - alpha/2, n)  #calculate t statistic
+  # #  calculate binomial standard deviation
+  # if (is.na(Np)){
+  #   sigma = sqrt(n*probs*(1-probs))
+  # }else if(!is.na(Np)){
+  #   fpc = sqrt ((Np - n) / (Np - 1))
+  #   sigma = sqrt(n*probs*(1-probs))*fpc
+  # }
+  # # estimate percentiles
+  # phi = log2(cfd[[1]])
+  # X = cfd[[2]]
+  # estimate = 2^approx(x = X, y = phi, xout = probs, rule = 2)[[2]]
+  # p.upper = (n*probs + tcrit*sigma)/n
+  # upper = 2^approx(x = X, y = phi, xout = p.upper, rule = 2)[[2]]
+  # p.lower = (n*probs - tcrit*sigma)/n
+  # lower = 2^approx(x = X, y = phi, xout = p.lower, rule = 2)[[2]]
+  
+  # use the binomial approach
+  p.upper = vector(mode="numeric", length = length(probs))
+  p.lower = vector(mode="numeric", length = length(probs))
+
+  if (equaltail == T){
+    for(i in seq_along(probs)){
+      tmp = QuantBD(n, probs[i], alpha)
+      p.upper[i] = tmp$equaltail[2]/n
+      p.lower[i] = tmp$equaltail[1]/n
+    }
+  }else{
+    for(i in seq_along(probs)){
+      tmp = QuantBD(n, probs[i], alpha)
+      p.upper[i] = tmp$interval[2]/n
+      p.lower[i] = tmp$interval[1]/n
+    }
   }
   # estimate percentiles
   phi = log2(cfd[[1]])
   X = cfd[[2]]
   estimate = 2^approx(x = X, y = phi, xout = probs, rule = 2)[[2]]
-  p.upper = (n*probs + tcrit*sigma)/n
   upper = 2^approx(x = X, y = phi, xout = p.upper, rule = 2)[[2]]
-  p.lower = (n*probs - tcrit*sigma)/n
   lower = 2^approx(x = X, y = phi, xout = p.lower, rule = 2)[[2]]
-
+  
   x.poly = c(upper, rev(lower))
   y.poly = c(probs, rev(probs))
   poly.out = data.frame(x.poly, y.poly)
